f1d875e5425b0274d9b6d1163738edc4af9e647e
Emil Yordanov <emil.yordanov@lights.digital>
2021-11-09 02:43:58 +0200

    Minor changes

--- React-UI/dj-api/src/components/FormErrors.js
+++ React-UI/dj-api/src/components/FormErrors.js
@@ -3,12 +3,13 @@ import React from 'react';
 export const FormErrors = ({formErrors}) =>
 <div className='formErrors'>
     {Object.keys(formErrors).map((fieldName, i) => {
-        if(formErrors[fieldName].length > 0){
-        return (
-            <p style={{color:"red", fontWeight:"bold"}} key={i}>{fieldName} {formErrors[fieldName]}</p>
-        )        
-        } else {
-        return '';
+        if (formErrors[fieldName].length > 0) {
+            return (
+                <p style={{color:"red", fontWeight:"bold"}} key={i}>{fieldName} {formErrors[fieldName]}</p>
+            )        
+        } 
+        else {
+            return '';
         }
     })}
 </div>
